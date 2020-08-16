bf3f53af2be428f7f393b575fa24db8935929fed
Alliques <airat1100@bk.ru>
2020-08-16 18:55:25 +0300

    added page for selecting required storage

--- CloudManager.Core/ViewModels/AddingDriveViewModel.cs
+++ CloudManager.Core/ViewModels/AddingDriveViewModel.cs
@@ -1,8 +1,43 @@
 ﻿
 
+using System.Windows.Input;
+
 namespace CloudManager.Core
 {
+    public enum StorageType
+    {
+        None,
+        GooleDrive,
+        OneDrive,
+        YandexDisk
+    }
     public class AddingDriveViewModel : BaseViewModel
     {
+        public AddingDriveViewModel()
+        {
+            SelectDriveCommand = new RelayParameterizedCommand((obj) => SelectDrive(obj));
+        }
+        private void SelectDrive(object obj)
+        {
+            switch ((StorageType)obj)
+            {
+                case StorageType.None:
+                    break;
+                case StorageType.GooleDrive:
+                    
+                    break;
+                case StorageType.OneDrive:
+                    
+                    break;
+                case StorageType.YandexDisk:
+                   
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        public ICommand SelectDriveCommand { get; set; }
     }
 }
